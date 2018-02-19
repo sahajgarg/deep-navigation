d0c8bcccca299e6350da161061a0f82ead2aed07
Ubuntu <ubuntu@ip-172-31-8-131.us-west-2.compute.internal>
2018-02-19 12:00:58 +0000

    fixed shot selection

--- select_sequences.py
+++ select_sequences.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from random import random
 
 LEN_OF_SEQ = 100
 SEQUENCE_NUMBERS = range(1, 11)
@@ -14,17 +15,17 @@ BASE_PATH = "dataset/sequences/"
 with open("sequences.csv", "a") as sequences:
 	for i in range(NUM_SEQ_TO_GENERATE):
 		# Randomly pick an index between 0, len(SEQUENCE_NUMBERS)
-		seq = int(rand() * NUM_SEQS)
+		seq = int(random() * NUM_SEQS)
 
 		# "Navingate" into the directory, check the number of lines in times.txt
-		num_lines = sum(1 for line in open(BASE_PATH + "{:2d}".format(seq)))
+		num_lines = sum(1 for line in open(os.path.join(BASE_PATH, "{:02d}".format(SEQUENCE_NUMBERS[seq]), "times.txt")))
 
 		# Sample a point between 0 and 100 before the last time in times.txt
-		start_frame = int(rand() * (num_lines - LEN_OF_SEQ)) - 1
+		start_frame = int(random() * (num_lines - LEN_OF_SEQ)) - 1
 
 		# Randomly pick image_2, or image_3
-		camera = int(rand() * 2) + 2
+		camera = int(random() * 2) + 2
 
 		# Append line to file: "{:2d},{},{:5d},{}".format(seq,camera,start_frame,LEN_OF_SEQ)
-		sequences.append("{:2d},image_{:1d},{:5d},{}".format(seq,camera,start_frame,LEN_OF_SEQ))
+		sequences.write("{:02d},image_{:1d},{:05d},{}\n".format(SEQUENCE_NUMBERS[seq],camera,start_frame,LEN_OF_SEQ))
 
